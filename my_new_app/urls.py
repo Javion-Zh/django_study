70a6f4707d16fcee10538e5f7d43c2f23ff6f64d
张佳旺 <ex_zhangjw6@cn.midea.com>
2020-10-12 10:11:50 +0800

    添加vscode配置--去除模板中的硬编码部分

--- my_new_app/urls.py
+++ my_new_app/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = my_new_app
 urlpatterns = [
-    # ex: /polls/
+    # ex: /my_new_app/
     path('', views.index, name='index'),
-    # ex: /polls/5/
+    # ex: /my_new_app/5/
     path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
+    # ex: /my_new_app/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
+    # ex: /my_new_app/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- my_new_web/settings.py
+++ my_new_web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'a2@jc48-l18o)$vpxy&hbn20&apo7wzh41qy_#7b*nw07_u#+3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False   
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
