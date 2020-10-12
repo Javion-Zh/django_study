47e94729c64db9a61c90aa6118607973bff086a0
Javion-Zh <1905015734@qq.com>
2020-10-12 23:40:03 +0800

    使用通用视图重构

--- my_new_app/urls.py
+++ my_new_app/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = 'my_new_app'
 urlpatterns = [
     # ex: /my_new_app/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: /my_new_app/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /my_new_app/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /my_new_app/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- my_new_app/views.py
+++ my_new_app/views.py
@@ -3,28 +3,39 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from .models import Question, Choice
+from django.views import generic
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'my_new_app/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'my_new_app/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        '''Return the last five published questions'''
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'my_new_app/detail.html', {'question': question})
+# def detail(request, question_id):
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'my_new_app/detail.html', {'question': question})
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'my_new_app/results.html', {'question': question})
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'my_new_app/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'my_new_app/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'my_new_app/results.html'
 
 
 def vote(request, question_id):
